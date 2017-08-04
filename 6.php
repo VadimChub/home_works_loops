d136ed310e79b936443c0c462244a5d4560f8bed
VadimChub <Vadimchub.92@gmail.com>
2017-08-04 12:13:24 +0300

    added new files up to 12.php

---
+++ 6.php
@@ -0,0 +1,11 @@
+<?php
+
+$arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
+
+foreach ($arr as $rus => $eng) {
+    $ru[] = $rus;
+    $en[] = $eng;
+}
+
+var_dump($ru);
+var_dump($en);
