b34e3a779d96cc7f50d0539bafbcd107888c07f8
VadimChub <Vadimchub.92@gmail.com>
2017-08-11 14:38:24 +0300

    modified tasks  10 and 26

--- 26.php
+++ 26.php
@@ -3,12 +3,14 @@
 for ($i = 0; $i < 20; $i++){
     $arr [] = rand(1,100);
 }
+
 $sum = 1;
-for ($i = 0; $i < sizeof($arr); $i++) {
+for ($i = 1; $i < sizeof($arr); $i++) {
     if ($arr[$i] > 0 && $i%2 == 0) {
         $sum = $sum * $arr[$i];
     }
 }
+
 
 for ($i = 0; $i < sizeof($arr); $i++) {
     if ($arr[$i] > 0 && $i%2 !== 0){
